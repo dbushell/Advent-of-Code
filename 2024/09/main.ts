fa377ff97189205c479787d9b7ec8c51e1238239
David Bushell <david@dbushell.com>
2024-12-12 14:52:49 +0000

    day 9 part 2

--- 2024/09/main.ts
+++ 2024/09/main.ts
@@ -1,8 +1,8 @@
-#!/usr/bin/env -S deno run --allow-read --allow-write
+#!/usr/bin/env -S deno run --allow-read
 import { assert } from "jsr:@std/assert/assert";
 
 const inputText = (await Deno.readTextFile(
-  new URL("test2.txt", import.meta.url),
+  new URL("input.txt", import.meta.url),
 )).trim();
 
 assert(inputText.length % 2, "Input must be odd");
@@ -13,7 +13,7 @@ const checksum = (array: typeof diskArray): number => {
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     const value = array[i];
-    if (typeof value === "string") break;
+    if (typeof value === "string") continue;
     sum += value * i;
   }
   return sum;
@@ -37,4 +37,37 @@ for (let i = diskOne.length - 1; i > 0; i--) {
 }
 const answerOne = checksum(diskOne);
 
+// Part 2
+const diskTwo = [...diskArray];
+for (let i = diskTwo.length - 1, size = 1; i > 1; i--, size++) {
+  const id = diskTwo[i];
+  // Reset and look for next file
+  if (id === ".") {
+    size = 0;
+    continue;
+  }
+  // Continue until entire file is found
+  if (diskTwo[i - 1] === id) continue;
+  // Look for free space
+  for (let j = 0, free = 0; j < i; j++) {
+    if (diskTwo[j] !== ".") {
+      free = 0;
+      continue;
+    }
+    // Move file into free space
+    if (++free === size) {
+      // I hope this works because I've lost track...
+      for (let k = 0; size; k++, size--) {
+        diskTwo[1 + (j - size)] = id;
+        diskTwo[k + i] = ".";
+      }
+      break;
+    }
+  }
+  // Reset
+  size = 0;
+}
+const answerTwo = checksum(diskTwo);
+
 console.log(`Answer 1: ${answerOne}`);
+console.log(`Answer 2: ${answerTwo}`);
