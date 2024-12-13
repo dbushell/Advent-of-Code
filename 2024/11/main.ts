8b488b4b4912c60b2bcd6b5f604530f44f4d0bd6
David Bushell <david@dbushell.com>
2024-12-13 13:43:54 +0000

    day 11 part 2

--- 2024/11/main.ts
+++ 2024/11/main.ts
@@ -35,4 +35,40 @@ for (let i = 0; i < 25; i++) {
 }
 const answerOne = stones.length;
 
+/*************
+ * PART TWO! *
+ *************/
+
+const stone2 = inputText.split(/\s+/)
+  .filter((s) => s.trim() !== "")
+  .map((s) => Number.parseInt(s));
+
+const cache2 = new Map<string, number>();
+
+const blink2 = (value: number, times: number): number => {
+  const key = `${value}-${times}`;
+  if (cache2.has(key)) return cache2.get(key)!;
+  if (times === 0) return 1;
+  times--;
+  let newValue = 0;
+  if (value === 0) {
+    newValue = blink2(1, times);
+  } else if (String(value).length % 2 !== 0) {
+    newValue = blink2(value * 2024, times);
+  } else {
+    const digits = String(value);
+    const v1 = Number.parseInt(digits.substring(0, digits.length / 2));
+    const v2 = Number.parseInt(digits.substring(digits.length / 2));
+    newValue = blink2(v1, times) + blink2(v2, times);
+  }
+  cache2.set(key, newValue);
+  return newValue;
+};
+
+let answerTwo = 0;
+for (const stone of stone2) {
+  answerTwo += blink2(stone, 75);
+}
+
 console.log(`Answer 1: ${answerOne}`);
+console.log(`Answer 2: ${answerTwo}`);
