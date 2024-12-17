b5fc6b3592467640124ba8ecf51455044a963238
David Bushell <david@dbushell.com>
2024-12-17 17:15:52 +0000

    2015 day 4

---
+++ 2015/04/main.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S deno run --allow-read
+import { crypto } from "jsr:@std/crypto/crypto";
+import { encodeHex } from "jsr:@std/encoding/hex";
+
+const inputText = (await Deno.readTextFile(
+  new URL("input.txt", import.meta.url),
+)).trim();
+
+const encoder = new TextEncoder();
+
+{
+  for (let i = 0; i < 1e6; i++) {
+    const hash = encodeHex(
+      await crypto.subtle.digest("MD5", encoder.encode(`${inputText}${i}`)),
+    );
+    if (hash.startsWith("00000")) {
+      console.log(`Answer 1: ${i}`);
+      break;
+    }
+  }
+}
+
+{
+  for (let i = 0; i < 1e7; i++) {
+    const hash = encodeHex(
+      await crypto.subtle.digest("MD5", encoder.encode(`${inputText}${i}`)),
+    );
+    if (hash.startsWith("000000")) {
+      console.log(`Answer 2: ${i}`);
+      break;
+    }
+  }
+}
